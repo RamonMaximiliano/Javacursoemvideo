311bb7cbb2a37618af61468788a93c7661787fad
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-04-12 21:30:11 -0300

    Javastart

---
+++ FirstAlgoritmoClass/src/firstalgoritmoclass/ContaBanco.java
@@ -0,0 +1,28 @@
+package firstalgoritmoclass;
+
+public class ContaBanco {
+
+    //Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    //Métodos personalizados
+    public void abrirConta() {
+    }
+
+    public void fecharConta() {
+    }
+
+    public void depositar() {
+    }
+
+    public void sacar() {
+    }
+
+    public void pagarMensal() {
+    }
+
+}
---
+++ FirstAlgoritmoClass/src/firstalgoritmoclass/FirstAlgoritmoClass.java
@@ -0,0 +1,11 @@
+
+package firstalgoritmoclass;
+
+public class FirstAlgoritmoClass {
+
+
+    public static void main(String[] args) {
+
+    }
+    
+}

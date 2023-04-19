acf37a55f6f7b8eef07308bc88eb9576bda8560a
Wilbert <ramon.wilbert@accruent.com>
2023-04-19 11:33:53 -0300

    almost

--- FirstAlgoritmoClass/src/firstalgoritmoclass/ContaBanco.java
+++ FirstAlgoritmoClass/src/firstalgoritmoclass/ContaBanco.java
@@ -10,19 +10,66 @@ public class ContaBanco {
     private boolean status;
 
     //Métodos personalizados
-    public void abrirConta() {
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+        if (t == "CC") {
+            this.setSaldo(50);
+        } else if (t == "CP") {
+            this.setSaldo(150);
+        }
+        System.out.println("Conta ABERTA com sucesso!");
     }
 
     public void fecharConta() {
+        if (this.saldo > 0) {
+            System.out.println("Conta possui saldo!");
+        } else if (this.saldo < 0) {
+            System.out.println("Saldo negativo!");
+        } else {
+            this.setStatus(false);
+        }
+        System.out.println("Conta ENCERRADA com sucesso!");
     }
 
-    public void depositar() {
+    public void depositar(float v) {
+        if (this.status == true) {
+            setSaldo(this.getSaldo() + v);
+        } else {
+            System.out.println("Impossível depositar!");
+        }
     }
 
-    public void sacar() {
+    public void sacar(float v) {
+        if (this.status == true) {
+            if (this.saldo >= v) {
+                setSaldo(this.saldo - v);
+                System.out.println("Saldo de " + v + "realizado na conta!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+        } else {
+            System.out.println("Impossível sacar!");
+        }
     }
 
     public void pagarMensal() {
+        float value = 0f;
+        if (this.tipo == "CC") {
+            value = 12f;
+        } else if (this.tipo == "CP") {
+            value = 20f;
+        }
+        if (this.status == true) {
+            if (this.saldo > value) {
+                setSaldo(getSaldo() - value);
+                System.out.println("Mensalidade paga com sucesso!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+        } else {
+            System.out.println("Impossível pagar!");
+        }
     }
 
     //Métodos especiais
@@ -76,6 +123,7 @@ public class ContaBanco {
 
 /*
 https://www.youtube.com/watch?v=hOC461osYgk&list=PLHz_AreHm4dkqe2aR0tQK74m8SFe-aGsY&index=11&ab_channel=CursoemV%C3%ADdeo
-*/
 
+- Criar método mostrar estado atual 19 min
 
+ */

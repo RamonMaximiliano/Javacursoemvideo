7b2105896ab24791afd4ed3bf3744d2cac9f11d3
Wilbert <ramon.wilbert@accruent.com>
2023-04-27 11:40:40 -0300

    interface finished

--- Interface/src/pkginterface/ControleRemoto.java
+++ Interface/src/pkginterface/ControleRemoto.java
@@ -1,13 +1,13 @@
 package pkginterface;
 
-public class ControleRemoto implements Controlador{
+public class ControleRemoto implements Controlador {
 
     private int volume;
     private boolean ligado;
     private boolean tocando;
 
     public ControleRemoto() {
-        this.volume = 50;
+        this.volume = 10;
         this.ligado = false;
         this.tocando = false;
     }
@@ -34,50 +34,73 @@ public class ControleRemoto implements Controlador{
 
     public void setTocando(boolean tocando) {
         this.tocando = tocando;
-    }  
+    }
 
+    //Métodos abstratos
     @Override
     public void ligar() {
+        this.setLigado(true);
     }
 
     @Override
     public void desligar() {
+        this.setLigado(false);
     }
 
     @Override
     public void abrirMenu() {
+        System.out.println("Esta ligado? " + this.isLigado());
+        System.out.println("Esta tocando? " + this.isTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i += 1) {
+            System.out.println("|");
+        }
     }
 
     @Override
     public void fecharMenu() {
+        System.out.println("Fechando Menu");
     }
 
     @Override
     public void maisVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() + 1);
+        }
     }
 
     @Override
     public void menosVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() - 1);
+        }
     }
 
     @Override
     public void ligarMudo() {
+        if (this.isLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
     }
 
     @Override
     public void desligarMudo() {
+        if (this.isLigado() && this.getVolume() == 0) {
+            this.setVolume(10);
+        }
     }
 
     @Override
     public void play() {
+        if (this.isLigado() && !(this.isTocando())) {
+            this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
+        if (this.isLigado() && (this.isTocando())) {
+            this.setTocando(false);
+        }
     }
 }
-
-
-
-
-

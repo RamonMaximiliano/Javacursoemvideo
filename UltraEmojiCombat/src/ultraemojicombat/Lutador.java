aa1c26aea2e46efe31ee2864aac2f76f3eaeed47
Wilbert <ramon.wilbert@accruent.com>
2023-05-10 10:05:45 -0300

    new class

---
+++ UltraEmojiCombat/src/ultraemojicombat/Lutador.java
@@ -0,0 +1,34 @@
+package ultraemojicombat;
+
+public class Lutador {
+
+    private String nome;
+    private String nacionalidade;
+    private String categoria;
+    private int idade;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    private float altura;
+    private float peso;
+
+    //métodos
+    public void apresentar() {
+
+    }
+
+    ;
+    
+    public void ganharLuta() {
+
+    }
+
+    public void perderLuta() {
+
+    }
+
+    public void empatarLuta() {
+
+    }
+
+}

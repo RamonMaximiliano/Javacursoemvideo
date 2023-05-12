09eba4e7109905b440c443756354a90e1dc3e2ab
Wilbert <ramon.wilbert@accruent.com>
2023-05-12 14:38:49 -0300

    one more

--- UltraEmojiCombat/src/ultraemojicombat/Lutador.java
+++ UltraEmojiCombat/src/ultraemojicombat/Lutador.java
@@ -14,20 +14,46 @@ public class Lutador {
 
     //métodos
     public void apresentar() {
+        System.out.println("Lutador: " + getNome());
+        System.out.println("Origem: " + getNacionalidade());
+        System.out.println("Idade: " + getIdade());
+        System.out.println("Altura: " + getAltura());
+        System.out.println("Peso: " + getPeso());
+        System.out.println("Vitórias: " + getVitorias());
+        System.out.println("Derrotas: " + getDerrotas());
+        System.out.println("Empates: " + getEmpates());
+    }
+
+    ;
+    
+    public void status() {
+        System.out.println("STATUS:");
+        System.out.println(getNome());
+        System.out.println(getCategoria());
+        System.out.println(getVitorias());
+        System.out.println(getDerrotas());
+        System.out.println(getEmpates());
     }
 
     ;
     
     public void ganharLuta() {
+        this.setVitorias(getVitorias() + 1);
     }
 
     ;
 
     public void perderLuta() {
+        this.setVitorias(getDerrotas() + 1);
     }
 
+    ;
+
     public void empatarLuta() {
+        this.setVitorias(getEmpates() + 1);
     }
+
+    ;
 
     //método construtor
     public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
@@ -35,9 +61,96 @@ public class Lutador {
         this.nacionalidade = nacionalidade;
         this.idade = idade;
         this.altura = altura;
-        this.peso = peso;
+        this.setPeso(peso);
         this.vitorias = vitorias;
         this.derrotas = derrotas;
     }
-;
+
+    ;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if (this.peso < 52.2) {
+            this.categoria = "Inválido";
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9) {
+            this.categoria = "Médio";
+        } else if (this.peso <= 120.2) {
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    ;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
 }
--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -1,12 +1,26 @@
-
 package ultraemojicombat;
-
 
 public class UltraEmojiCombat {
 
-
     public static void main(String[] args) {
+        Lutador L1 = new Lutador("Namaste", "França", 35, 1.85f, 85f, 15, 2, 1);
 
+        //variavel declarada em format vetor
+        Lutador l[] = new Lutador[2];
+
+        l[0] = new Lutador("John", "Brasil", 29, 1.65f, 60f, 18, 1, 0);
+        l[1] = new Lutador("Roger", "EUA", 25, 1.61f, 69f, 10, 0, 1);
+
+        l[0].status();
+        System.out.println("APRESENTAR");
+        l[0].apresentar();
+        System.out.println("--------------------------");
+        l[1].status();
+        System.out.println("APRESENTAR");
+        l[1].apresentar();
+        System.out.println("--------------------------");
+        L1.status();
+        System.out.println("APRESENTAR");
+        L1.apresentar();
     }
-    
-}
+};

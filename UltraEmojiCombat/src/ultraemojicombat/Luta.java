f0dd44126c79817f0c70fe38bc1122ab73da53b7
Wilbert <ramon.wilbert@accruent.com>
2023-05-22 17:48:59 -0300

    few

---
+++ UltraEmojiCombat/src/ultraemojicombat/Luta.java
@@ -0,0 +1,20 @@
+
+package ultraemojicombat;
+
+
+public class Luta {
+    //atributos
+    private Lutador desafiante;
+    private Lutador desafiado;
+    private int rounds;
+    private boolean aprovada;
+    
+    //Métodos publicos
+    public void marcarLuta(){
+    
+    };
+    public void Lutar(){
+    
+    };
+    
+}

ac7af863e505b5f901ca5e99a68d4d612a5a4f26
RamonMaximiliano <ramon_max1986@hotmail.com>
2023-06-28 18:46:03 -0300

    updates

---
+++ ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
@@ -0,0 +1,9 @@
+package projetoyoutube;
+
+public class ProjetoYouTube {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ ProjetoYouTube/src/projetoyoutube/Video.java
@@ -0,0 +1,26 @@
+package projetoyoutube;
+
+public class Video implements AcoesVideo {
+
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void like() {
+
+    }
+
+}

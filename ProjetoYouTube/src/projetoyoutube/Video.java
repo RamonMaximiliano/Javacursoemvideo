54bd9de0eaf016a4212a761c20cdae759e3ec74a
RamonMaximiliano <ramon_max1986@hotmail.com>
2023-06-30 15:10:13 -0300

    test

--- ProjetoYouTube/src/projetoyoutube/Video.java
+++ ProjetoYouTube/src/projetoyoutube/Video.java
@@ -58,22 +58,21 @@ public class Video implements AcoesVideo {
 
     @Override
     public void play() {
-
+        this.reproduzindo = true;
     }
 
     @Override
     public void pause() {
-
+        this.reproduzindo = false;
     }
 
     @Override
     public void like() {
-
+        this.setCurtidas(this.getCurtidas()+1);
     }
 
     @Override
     public String toString() {
         return "Video{" + "titulo = " + titulo + ", avaliacao = " + avaliacao + ", views = " + views + ", curtidas = " + curtidas + ", reproduzindo = " + reproduzindo + '}';
     }
-
 }

4ba67696af8ac82d53ba7180bff1c52f1545abbb
RamonMaximiliano <ramon_max1986@hotmail.com>
2023-07-18 12:20:14 -0300

    debugging

--- Master1/src/master1/Master1.java
+++ Master1/src/master1/Master1.java
@@ -5,7 +5,13 @@ public class Master1 {
 
 
     public static void main(String[] args) {
+        //This is a simples debugging exercise
         System.out.println("Hello world!");
+        System.out.println("Step 1!");
+        System.out.println("Step 2!");
+        System.out.println("Step 3!");
+        System.out.println("Step 4!");
+        System.out.println("Step 5!");
     }
     
 }
